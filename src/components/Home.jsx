80526d6d65156101b30d77ebd3b19e5519283cef
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-04-28 16:38:51 -0700

    done except media queries

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,21 +5,17 @@ import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Grid from "@material-ui/core/Grid";
-import { Link } from 'react-router-dom'
-// import Selene from './components/Selene'
-// import Lavender from './components/Lavender'
-// import Farmony from './components/Farmony'
+
 
 const Home = () => {
-    const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+    
     return (
         <div className="home-page">
             
-                <Grid container className="home-head">
-                <Grid item sm={12}><h1 className="name">Sophia Di Paola</h1></Grid>
-                <Grid item sm={12}><h5 className="front">Front-End Developer</h5></Grid>
-                </Grid>
+                <div className="home-head">
+                <h1 className="name">Sophia Di Paola</h1>
+                <h5 className="front">Front-End Developer</h5>
+                </div>
                 
            
             

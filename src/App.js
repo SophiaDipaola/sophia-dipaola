c4e3de7f30f9c13b8dd21aec2bdc60b22811a18c
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-04-29 19:34:58 -0700

    deply debugging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 
 import './App.css';
-import Home from './components/Home'
+
 import Navbar from './components/Navbar'
 import routes from './config/routes'
 
-import { Route } from 'react-router-dom';
+
 
 
 function App() {
--- src/components/ProjectsCarousel.jsx
+++ src/components/ProjectsCarousel.jsx
@@ -9,19 +9,19 @@ const ProjectsCarousel = () => {
             <div className="caro">
             <Carousel>
                 <div>
-                    <img className="port-img" src="https://i.imgur.com/wHKJagx.png" />
+                    <img className="port-img" src="https://i.imgur.com/wHKJagx.png" alt="farmony-home"/>
                     <a href="/farmony" className="legend">Learn more about Farmony</a>
                 </div>
                 <div>
-                    <img className="port-img" src="https://i.imgur.com/SqoDhkg.pngg" />
+                    <img className="port-img" src="https://i.imgur.com/SqoDhkg.pngg" alt="selene-home"/>
                     <a href="/selene" className="legend">Learn more about Selene</a>
                 </div>
                 <div>
-                    <img className="port-img" src="https://i.imgur.com/7xybxnL.jpg" />
+                    <img className="port-img" src="https://i.imgur.com/7xybxnL.jpg" alt="mindwholeness-home"/>
                     <a href="/mindwholeness" className="legend">Learn more about The MindWholeness Podcast</a>
                 </div>
                 <div>
-                    <img className="port-img" src="https://i.imgur.com/PeWSamq.png" />
+                    <img className="port-img" src="https://i.imgur.com/PeWSamq.png" alt="lavender-recovery-home" />
                     <a href="/lavender" className="legend">Learn more about Lavender Recovery</a>
                 </div>
             </Carousel>

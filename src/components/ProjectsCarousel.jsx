83eeb0b02218b0828cacd2de31073056bd2ded86
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-05-05 12:16:33 -0700

    trying to fix broken ind projects links

--- src/components/ProjectsCarousel.jsx
+++ src/components/ProjectsCarousel.jsx
@@ -9,19 +9,19 @@ const ProjectsCarousel = () => {
             <Carousel>
                 <div>
                     <img className="port-img" src="https://i.imgur.com/wHKJagx.png" alt="farmony-home"/>
-                    <a href="https://www.sophia-dipaola.com/farmony" className="legend">Learn more about Farmony</a>
+                    <a href="http://sophia-dipaola.herokuapp.com/farmony" className="legend">Learn more about Farmony</a>
                 </div>
                 <div>
                     <img className="port-img" src="https://i.imgur.com/SqoDhkg.pngg" alt="selene-home"/>
-                    <a href="https://www.sophia-dipaola.com/selene" className="legend">Learn more about Selene</a>
+                    <a href="http://sophia-dipaola.herokuapp.com/selene" className="legend">Learn more about Selene</a>
                 </div>
                 <div>
                     <img className="port-img" src="https://i.imgur.com/7xybxnL.jpg" alt="mindwholeness-home"/>
-                    <a href="https://www.sophia-dipaola.com/mindwholeness" className="legend">Learn more about The MindWholeness Podcast</a>
+                    <a href="http://sophia-dipaola.herokuapp.com/mindwholeness" className="legend">Learn more about The MindWholeness Podcast</a>
                 </div>
                 <div>
                     <img className="port-img" src="https://i.imgur.com/PeWSamq.png" alt="lavender-recovery-home" />
-                    <a href="https://www.sophia-dipaola.com/lavender" className="legend">Learn more about Lavender Recovery</a>
+                    <a href="http://sophia-dipaola.herokuapp.com/lavender" className="legend">Learn more about Lavender Recovery</a>
                 </div>
             </Carousel>
             </div>
